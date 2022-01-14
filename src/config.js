17f702eb11bd62d96929839a1b71d9757e518006
ethanruity <eruity1@gmail.com>
2022-01-14 11:14:37 -0500

    now done?

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
 "use strict";
-console.log(process.env.PORT);
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.DB_URL,
-  // "mongodb+srv://moose:ondal00se@hgcluster.bhim4.mongodb.net/hgcluster?authSource=admin&replicaSet=atlas-mx3kc1-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true",
-  //DATABASE_URL: `mongodb+srv://moose:${process.env.DB_PASSWORD}@hgcluster.bhim4.mongodb.net/hgcluster?authSource=admin&replicaSet=atlas-mx3kc1-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true`,
 };
--- src/config/keys.js
+++ src/config/keys.js
@@ -1,15 +1,15 @@
 keys = {
   app: {
-    name: 'Hidden Gems API',
-    apiEndpoint: (process.env.API_URL) ? `/${process.env.API_URL}` : '/api',
+    name: "Hidden Gems API",
+    apiEndpoint: process.env.API_URL ? `/${process.env.API_URL}` : "/api",
   },
   database: {
-    url: process.env.MONGODB_URI || `"mongodb+srv://moose:ondal00se@hgcluster.bhim4.mongodb.net/HGCluster?retryWrites=true&w=majority"`,
+    url: `"mongodb+srv://moose:${process.env.DB_PASSWORD}@hgcluster.bhim4.mongodb.net/HGCluster?retryWrites=true&w=majority"`,
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'jwt-secret',
-    tokenLife: '7d',
+    secret: process.env.JWT_SECRET || "jwt-secret",
+    tokenLife: "7d",
   },
-}
+};
 
 module.exports = keys;
